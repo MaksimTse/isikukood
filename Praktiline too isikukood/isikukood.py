6c8fffaf735b4836b07438708a09561d7c6502cb
MaksimTse <120181800+MaksimTse@users.noreply.github.com>
2023-02-06 13:40:45 +0200

    Update isikukood.py

--- Praktiline too isikukood/isikukood.py
+++ Praktiline too isikukood/isikukood.py
@@ -4,10 +4,14 @@ from OmaMoodul import *
 
 firstdigit=[1,2,3,4,5,6]
 ikood=""
+arvud=["100","1001","211","222"]
+ikoodid=[]
+
 while True:
     ikood=input("sisestage sinu isikukood: \n")
     if checklen(ikood)==False:
         print("Liiga pikk või lühike isikukood")
+        arvud.append(ikood)
     else:
         s=sugu(ikood)
         if s=="viga":
@@ -19,6 +23,11 @@ while True:
                 print("2-7 tähed on valesti sisestatud")
             else:
                 print("------------------------------")
-                print(lause(ikood))
-
+                lause(ikood)
+                print("------------------------------")
+                print((kontrollnr(ikood)))
+                if kontrollnr(ikood)==int(ikood[-1]):
+                    print("OK")
+                else:
+                    print("!!!")
                 
